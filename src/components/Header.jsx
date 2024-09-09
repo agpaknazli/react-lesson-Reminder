3b9a43155530826fe23eed575c75d084f3b1eb7e
agpaknazli <agpaknazli19@gmail.com>
2024-09-10 01:04:57 +0200

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,16 @@ import icon from '../image/icon.jpg';
 
 const Header = () => {
   return (
-    <div className='header d-flex'>
-        <img className='' src={icon} alt="icon" />
-        <h1>Clarusway</h1>
+    <div className="header d-flex align-items-center">
+        <img 
+          className='me-3' 
+          src={icon} 
+          alt="icon" 
+          style={{ width: '100px', height: '100px' }} 
+        />
+        <h1 className="mb-0">Clarusway</h1>
     </div>
-  )
+  );
 }
 
 export default Header;
--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,36 +1,29 @@
 import React from 'react'
 import { data } from '../helper/data';
+
 const LessonCard = () => {
   return (
-    <div>
-
-
-{data.map((item)=>{
-
-return(
-<div>
-<img src={item.image} alt="icon" />
-
-<div>
-<h4>Lesson Name: {item.name}</h4>
-<h4>Lesson Hour:{item.hour}</h4>
-
-</div>
-
-</div>
-);
-
-
-})
-}
-
-
-
-
-
-
+    <div className='container'>
+      <div className='row'>
+        {data.map((item, index) => {
+          return (
+            <div key={index} className='col-lg-6 col-12 d-flex align-items-center gap-3 mb-4'>
+              <img 
+                src={item.image} 
+                alt="icon" 
+                className="img-fluid" 
+                style={{ width: '100px', height: '100px' }} 
+              />
+              <div className='flex-grow-1'>
+                <h4>Lesson Name: {item.name}</h4>
+                <h4>Lesson Hour: {item.hour}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default LessonCard;
